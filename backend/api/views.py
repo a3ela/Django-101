a7d9e74ae7fa8e92c30832fc6ed09ebc814389ee
abel <abelsintayehu626@gmail.com>
2024-12-04 12:45:08 +0300

    feat: a feature for the get request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,26 @@
 from django.http import JsonResponse
 import json
+from django.forms.models import model_to_dict
+from products.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from products.serializers import ProductSerializer
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    body = request.body
+    instance = Product.objects.all().order_by('?').first()
     data = {}
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title'])
+        data = ProductSerializer(instance).data
+    return Response(data)
 
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(request.GET['abc'])
-    print(data)
-    data['params'] = dict(request.GET)
-    data['content-type'] = request.content_type
-    return JsonResponse(data)
+@api_view(['POST'])
+def api_home(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        print(serializer.data)
+        data = serializer.data
+        return Response(data)
