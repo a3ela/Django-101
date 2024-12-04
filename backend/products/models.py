6e33c033f1d23372454b2b6738337290572bbeaf
abel <abelsintayehu626@gmail.com>
2024-12-04 12:44:04 +0300

    File: for serializing our database content

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.TextField(blank=True, null=True, default=99.99)
+
+
+    @property
+    def sale_price(self):
+        return '{:.2f}'.format(float(self.price) * 0.8)
+    
+    def get_discount(self):
+        return '122'
